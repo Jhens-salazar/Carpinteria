cbbf4e56978117e618b48dbc78b12b079c10ddd7
David Mejia <mejia018000@gmail.com>
2023-10-11 23:56:31 -0500

    solucion ado

--- Login/controlador_login.php
+++ Login/controlador_login.php
@@ -1,13 +1,11 @@
 <?php
 
-
 // Verificar si el formulario de login ha sido enviado
 if (isset($_POST['sesion'])) {
     include('conexion.php');
     // Obtener los datos del formulario
     $username = $_POST['username'];
     $contrasena = $_POST['contrasena'];
-
     // Realizar la consulta a la base de datos para verificar las credenciales
     $query = "SELECT * FROM clientes WHERE username = '$username'";
     $result = mysqli_query($conn, $query);
@@ -24,16 +22,33 @@ if (isset($_POST['sesion'])) {
             $_SESSION['id_usuario'] = $row['id_cliente'];
             $_SESSION['username'] = $row['nombres'];
             $_SESSION['rol'] = $row['rol'];
-
+            echo $_SESSION['id_usuario'];	    
             if ($row['rol'] == 1) {
-                echo '<script>window.location.href = "../admin/admin.php";</script>';
+                echo '<script>window.location.href = "../assets/admin/admin.php";</script>';
             } else if ($row['rol'] == 2) {
                 echo '<script>window.location.href = "../index.php";</script>';
             } else {
                 echo '<script>window.location.href = "./index.php";</script>';
             }
             exit();
-        } else {
+        }elseif ($contrasena === $row['contrasena']) {
+            session_start();
+            // Guardar la información del usuario en la sesión
+            $_SESSION['id_usuario'] = $row['id_cliente'];
+            $_SESSION['username'] = $row['nombres'];
+            $_SESSION['rol'] = $row['rol'];
+            echo $_SESSION['id_usuario'];	    
+            if ($row['rol'] == 1) {
+                echo '<script>window.location.href = "../assets/admin/admin.php";</script>';
+            } else if ($row['rol'] == 2) {
+                echo '<script>window.location.href = "../index.php";</script>';
+            } else {
+                echo '<script>window.location.href = "./index.php";</script>';
+            }
+            exit();
+        } 
+        
+        else {
             // Si la contraseña no es válida, mostrar un mensaje de error
             echo "<script>Swal.fire('Contraseña incorrecta, Intentelo nuevamente')</script>";
         }
